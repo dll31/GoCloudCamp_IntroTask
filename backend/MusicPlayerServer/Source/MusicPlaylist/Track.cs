a017a21dd8e7a5df033485ab33bcfe20f0da49ce
dll31 <danila.litvinov31@yandex.ru>
2023-03-01 20:19:26 +0300

    add Track code

---
+++ backend/MusicPlayerServer/Source/MusicPlaylist/Track.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace MusicTrack;
+
+public class Track
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int Duration { get; set; }
+    public string Path { get; set; }
+
+    public Track(string name, string author, int duration, string path)
+    {
+        Name = name;
+        Author = author;
+        Duration = duration;
+        Path = path;
+    }
+}
+
