1be36aaf00ec1134dd2eb4594358be0e3d236e14
dll31 <danila.litvinov31@yandex.ru>
2023-03-01 20:19:50 +0300

    add emulator code

---
+++ backend/MusicPlayerServer/Source/MusicPlaylist/EmulatorPlayer.cs
@@ -0,0 +1,69 @@
+﻿
+
+namespace MusicEmulatorPlayer;
+
+public enum EmulatorPlayerErrors
+{
+    Canceled,
+    Timeout
+}
+
+
+public class EmulatorPlayer
+{
+    public int Duration { get; set; }
+
+    public Task Emulator { get; set; }
+
+    public CancellationTokenSource CTokenSource = new();
+    private CancellationToken CToken { get; set; }
+
+
+    public void RemakeCTokens()
+    {
+        CTokenSource = new CancellationTokenSource();
+        CToken = CTokenSource.Token;
+    }
+
+
+    public async Task StartTaskAsync(Action<EmulatorPlayerErrors> callback)
+    {
+
+        Console.WriteLine("Task restarted with delay " + Duration.ToString());
+        try
+        {
+            await Task.Delay(Duration, CToken);
+        }
+        catch (TaskCanceledException)
+        {
+            Console.WriteLine("Cancel request accepted");
+            callback(EmulatorPlayerErrors.Canceled);
+        }
+
+        Console.WriteLine("End of task by timeout");
+        callback(EmulatorPlayerErrors.Timeout);
+    }
+
+
+    public async void RestartTaskAsync(Action<EmulatorPlayerErrors> callback)
+    {
+        //ManualResetEventSlim pauseEvent = new(true);
+
+
+        if (Emulator != null && !Emulator.IsCompleted)
+        {
+            await Emulator;
+        }
+
+        Emulator = Task.Run(() =>
+        {
+            //pauseEvent.Wait();
+            StartTaskAsync(callback).Wait();
+        }, CToken);
+
+    }
+}
+
+
+
+
