d21927fc26c22a5f358aa9725277764bbed68dfb
dll31 <danila.litvinov31@yandex.ru>
2023-03-01 20:19:38 +0300

    add playlist code

---
+++ backend/MusicPlayerServer/Source/MusicPlaylist/Playlist.cs
@@ -0,0 +1,48 @@
+﻿using MusicTrack;
+
+
+namespace MusicPlaylist;
+
+public class Playlist
+{
+    public long Id { get; }
+    public string Name { get; set; }
+    private LinkedList<Track> playlist = new();
+
+
+    public LinkedListNode<Track> Next(ref LinkedListNode<Track> current)
+    {
+        return current.Next;
+    }
+
+
+    public LinkedListNode<Track> Prev(ref LinkedListNode<Track> current)
+    {
+        return current.Previous;
+    }
+    
+
+    public void Delete(ref LinkedListNode<Track> track)
+    {
+        playlist.Remove(track);
+    }
+
+
+    public void AddTrack(ref Track track)
+    {
+        playlist.AddLast(track);
+    }
+
+
+    public LinkedListNode<Track> GetBegin()
+    {
+        return playlist.First;
+    }
+
+
+    public int GetDuration(ref LinkedListNode<Track> current)
+    {
+        return current.Value.Duration;
+    }
+
+}
