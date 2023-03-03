6c6294ac03da87fde3e15c885abfdcbfa6188145
dll31 <danila.litvinov31@yandex.ru>
2023-03-03 17:41:22 +0300

    little main reworks

--- backend/MusicPlayerServer/Program.cs
+++ backend/MusicPlayerServer/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Hosting.Server;
 using MusicPlayerServer.Services;
+
 
 class Program
 {
@@ -16,6 +18,6 @@ class Program
         app.MapGrpcService<PlayerService>();
         app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-        app.Run();
+        app.Run("http://localhost:50051");
     }
 }
