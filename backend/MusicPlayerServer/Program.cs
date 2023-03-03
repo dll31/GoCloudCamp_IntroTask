17c2a1c7fa0857e8bcb2e9afe970012cf05f087a
dll31 <danila.litvinov31@yandex.ru>
2023-03-03 03:29:56 +0300

    clean needless comment in main

--- backend/MusicPlayerServer/Program.cs
+++ backend/MusicPlayerServer/Program.cs
@@ -66,39 +66,3 @@ class Program
         return;
     }
 }
-
-
-//class Program
-//{
-//    static async Task Main()
-//    {
-//        CancellationTokenSource cts = new CancellationTokenSource();
-
-//        Task task = Task.Run(async () =>
-//        {
-//            try
-//            {
-//                Console.WriteLine("Task started...");
-//                await Task.Delay(TimeSpan.FromSeconds(10), cts.Token);
-//                Console.WriteLine("Task completed.");
-//            }
-//            catch (TaskCanceledException)
-//            {
-//                Console.WriteLine("Task was canceled.");
-//            }
-//        });
-
-//        await Task.Delay(TimeSpan.FromSeconds(5));
-//        cts.Cancel();
-
-//        using (var ctr = cts.Token.Register(() =>
-//        {
-//            Console.WriteLine("Cancellation callback called.");
-//        }))
-//        {
-//            await task;
-//        }
-
-//        await Task.Delay(TimeSpan.FromSeconds(7));
-//    }
-//}
